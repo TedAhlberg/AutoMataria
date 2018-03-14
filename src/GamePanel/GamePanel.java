a0568572e4c2dff59cd96fb957940bd8575496a0
TedAhlberg <35498698+TedAhlberg@users.noreply.github.com>
2018-03-14 09:28:31 +0100

    Moving square

--- src/GamePanel/GamePanel.java
+++ src/GamePanel/GamePanel.java
@@ -11,25 +11,31 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel implements Runnable {
 	private static final int PWIDTH = 500;
 	private static final int PHEIGHT = 400;
-	
+
 	private volatile boolean running;
 
 	private Thread animator;
 	private int period = 10;
 	private int fps;
-	
+
+	private int x = 0;
+	private int y = 0;
+
 	private Graphics bg;
 	private Image bufferImage = null;
 
 	public GamePanel() {
 		setBackground(Color.WHITE);
 		setPreferredSize(new Dimension(PWIDTH, PHEIGHT));
+
+		setFocusable(true);
+		requestFocus();
 	}
 
-	public void addNotify() {
-		super.addNotify();
-		startGame();
-	}
+	 public void addNotify() {
+	 super.addNotify();
+	 startGame();
+	 }
 
 	private void startGame() {
 		if (animator == null || !running) {
@@ -45,37 +51,38 @@ public class GamePanel extends JPanel implements Runnable {
 	public void run() {
 		long beforeTime, diffTime, sleepTime;
 		beforeTime = System.currentTimeMillis();
-		
 		running = true;
-		while(running) {
+
+		while (running) {
 			gameUpdate();
 			gameRender();
 			paintScreen();
-			
+
 			diffTime = System.currentTimeMillis() - beforeTime;
 			sleepTime = period - diffTime;
-			
-			if(sleepTime <= 0) {
+
+			if (sleepTime <= 0) {
 				sleepTime = 5;
 			}
-			
+
 			try {
 				Thread.sleep(sleepTime);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			fps = (int) (sleepTime * 10);
 			beforeTime = System.currentTimeMillis();
 		}
 		System.exit(0);
 	}
-	
+
 	private void paintScreen() {
 		Graphics g;
 		try {
 			g = this.getGraphics();
-			if (g != null && bufferImage != null) 
+
+			if ((g != null) && (bufferImage != null))
 				g.drawImage(bufferImage, 0, 0, null);
 			Toolkit.getDefaultToolkit().sync();
 			g.dispose();
@@ -86,24 +93,28 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 
 	private void gameUpdate() {
-		
+		x++;
 	}
-	
+
 	private void gameRender() {
-		if(bufferImage == null) {
+		if (bufferImage == null) {
 			bufferImage = createImage(PWIDTH, PHEIGHT);
-			if(bufferImage == null) {
+
+			if (bufferImage == null) {
 				System.err.println("renderImage is null");
 				return;
+
 			} else {
 				bg = bufferImage.getGraphics();
 			}
-			//clear
-			bg.setColor(Color.white);
-			bg.fillRect(0, 0, PWIDTH, PHEIGHT);
 		}
+		bg.setColor(Color.WHITE);
+		bg.fillRect(0, 0, PWIDTH, PHEIGHT);
+
+		bg.setColor(Color.BLACK);
+		bg.fillRect(x, y, 5, 5);
 	}
-	
+
 	public int getWidth() {
 		return PWIDTH;
 	}
--- src/GamePanel/GameWindow.java
+++ src/GamePanel/GameWindow.java
@@ -1,18 +1,17 @@
 package GamePanel;
 
 import java.awt.Dimension;
+
 import java.awt.Point;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
-
 public class GameWindow extends JFrame {
-	private GamePanel panel = new GamePanel();
+	private GamePanel gamePanel = new GamePanel();
 
 	public GameWindow() {
-		add(panel);
+		add(gamePanel);
 		setLocation(getCenterLocation());
 		pack();
 		setVisible(true);
@@ -21,8 +20,8 @@ public class GameWindow extends JFrame {
 	private Point getCenterLocation() {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screen = toolkit.getScreenSize();
-		int x = (screen.width / 2) - (panel.getWidth() / 2);
-		int y = (screen.height / 2) - (panel.getHeight() / 2);
+		int x = (screen.width / 2) - (gamePanel.getWidth() / 2);
+		int y = (screen.height / 2) - (gamePanel.getHeight() / 2);
 		return new Point(x, y);
 	}
 
