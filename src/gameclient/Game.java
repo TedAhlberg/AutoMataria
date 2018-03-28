2f6497e42e85b589e42706b1da9014838621cffd
lundow <eriklundow@gmail.com>
2018-03-28 11:43:53 +0200

    Working audioplayer

--- src/gameclient/Game.java
+++ src/gameclient/Game.java
@@ -1,5 +1,6 @@
 package gameclient;
 
+import common.AudioPlayer;
 import common.GameObject;
 import gameserver.GameServer;
 
@@ -17,78 +18,83 @@ import java.util.Collection;
  * @author Johannes Bluml
  */
 public class Game extends Canvas {
-    public static final String TITLE = "Auto-Mataria";
-    private final double scale;
+	public static final String TITLE = "Auto-Mataria";
+	private final double scale;
 
-    private ClientConnection client;
-    private String serverIP = "localhost";
-    private int serverPort = 32000;
-    private BufferedImage background;
+	private ClientConnection client;
+	private String serverIP = "localhost";
+	private int serverPort = 32000;
+	private BufferedImage background;
+	private AudioPlayer backgroundMusic = AudioPlayer.getSound("resources/Music/AM-track1.wav");
 
-    public Game() {
-        String playerName = JOptionPane.showInputDialog("Enter your username:", "Username");
-        new Window(TITLE, this);
+	public Game() {
 
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        try {
-            env.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("resources/Orbitron Bold.ttf")));
-            background = ImageIO.read(new File("resources/Stars.png"));
-        } catch (IOException | FontFormatException e) {
-            System.out.println("Failed to load resources.");
-            e.printStackTrace();
-        }
+		String playerName = JOptionPane.showInputDialog("Enter your username:", "Username");
+		backgroundMusic.play();
+		new Window(TITLE, this);
 
-        Rectangle bounds = env.getDefaultScreenDevice().getDefaultConfiguration().getBounds();
-        scale = Math.min(bounds.getWidth() / GameServer.WIDTH, bounds.getHeight() / GameServer.HEIGHT);
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		try {
+			env.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("resources/Orbitron Bold.ttf")));
+			background = ImageIO.read(new File("resources/Stars.png"));
+		} catch (IOException | FontFormatException e) {
+			System.out.println("Failed to load resources.");
+			e.printStackTrace();
+		}
 
-        Graphics2D g = (Graphics2D) background.getGraphics();
-        g.scale(scale, scale);
-        g.setPaint(new Color(1, 1, 1, 0.2f));
-        for (int i = 0; i <= GameServer.WIDTH; i += GameServer.GRIDSIZE) {
-            g.drawLine(i, 0, i, GameServer.HEIGHT);
-        }
-        for (int i = 0; i <= GameServer.HEIGHT; i += GameServer.GRIDSIZE) {
-            g.drawLine(0, i, GameServer.WIDTH, i);
-        }
-        g.dispose();
+		Rectangle bounds = env.getDefaultScreenDevice().getDefaultConfiguration().getBounds();
+		scale = Math.min(bounds.getWidth() / GameServer.WIDTH, bounds.getHeight() / GameServer.HEIGHT);
 
-        try {
-            Socket socket = new Socket(serverIP, serverPort);
-            client = new ClientConnection(socket, gameObjects -> render((Collection<GameObject>) gameObjects));
-            client.send(playerName);
-            this.addKeyListener(new KeyInput(client));
-            this.requestFocus();
-        } catch (IOException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Unable to connect server.");
-            System.exit(1);
-        }
-    }
+		Graphics2D g = (Graphics2D) background.getGraphics();
+		g.scale(scale, scale);
+		g.setPaint(new Color(1, 1, 1, 0.2f));
+		for (int i = 0; i <= GameServer.WIDTH; i += GameServer.GRIDSIZE) {
+			g.drawLine(i, 0, i, GameServer.HEIGHT);
+		}
+		for (int i = 0; i <= GameServer.HEIGHT; i += GameServer.GRIDSIZE) {
+			g.drawLine(0, i, GameServer.WIDTH, i);
+		}
+		g.dispose();
 
-    public static int clamp(int var, int min, int max) {
-        if (var >= max) return max;
-        if (var <= min) return min;
-        return var;
-    }
+		try {
+			Socket socket = new Socket(serverIP, serverPort);
+			client = new ClientConnection(socket, gameObjects -> render((Collection<GameObject>) gameObjects));
+			client.send(playerName);
+			this.addKeyListener(new KeyInput(client));
+			this.requestFocus();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Unable to connect server.");
+			System.exit(1);
+		}
+	}
 
-    public static void main(String[] args) {
-        new Game();
-    }
+	public static int clamp(int var, int min, int max) {
+		if (var >= max)
+			return max;
+		if (var <= min)
+			return min;
+		return var;
+	}
 
-    private void render(Collection<GameObject> gameObjects) {
-        BufferStrategy bs = this.getBufferStrategy();
-        if (bs == null) {
-            this.createBufferStrategy(2);
-            return;
-        }
-        Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+	public static void main(String[] args) {
+		new Game();
+	}
 
-        g.drawImage(background, 0, 0, this);
-        g.scale(scale, scale);
-        gameObjects.forEach(gameObject -> gameObject.render(g));
+	private void render(Collection<GameObject> gameObjects) {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(2);
+			return;
+		}
+		Graphics2D g = (Graphics2D) bs.getDrawGraphics();
 
-        g.dispose();
-        bs.show();
-        Toolkit.getDefaultToolkit().sync();
-    }
+		g.drawImage(background, 0, 0, this);
+		g.scale(scale, scale);
+		gameObjects.forEach(gameObject -> gameObject.render(g));
+
+		g.dispose();
+		bs.show();
+		Toolkit.getDefaultToolkit().sync();
+	}
 }
