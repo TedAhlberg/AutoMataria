27210c0ce2f10339dcee3c63d64f93d2a9953ea1
lundow <eriklundow@gmail.com>
2018-04-06 18:55:33 +0200

    settingsScreen

--- src/gameclient/SettingsScreen.java
+++ src/gameclient/SettingsScreen.java
@@ -4,12 +4,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -22,8 +22,19 @@ public class SettingsScreen extends JPanel {
 	private JTextField tfName = new JTextField();
 	private Font fontHead = new Font("Orbitron", Font.BOLD, 50);
 	private Font fontText = new Font("Orbitron", Font.BOLD, 20);
-//	private JPanel pnlSettings = new JPanel(new BorderLayout());
-	
+	private Font fontTextSmall = new Font("Orbitron", Font.BOLD, 14);
+	private JLabel lblUserName = new JLabel("CHANGE USERNAME");
+	private JLabel lblSettings = new JLabel("SETTINGS");
+	private JLabel lblControls = new JLabel("CONTROLS");
+	private JLabel lblUp = new JLabel("UP : ARROWKEY_UP");
+	private JLabel lblLeft = new JLabel("LEFT : ARROWKEY_LEFT");
+	private JLabel lblDown = new JLabel("LEFT : ARROWKEY_DOWN");
+	private JLabel lblRight = new JLabel("RIGHT : ARROWKEY_RIGHT");
+
+	private String fileChangePressed = "images/SetKeyBinding_Pressed.png";
+	private String fileChangeUnpressed = "images/SetKeyBinding_Unpressed.png";
+
+	private Buttons btnChange = new Buttons(fileChangePressed, fileChangeUnpressed);
 
 	public SettingsScreen() {
 		JFrame frame = new JFrame();
@@ -40,26 +51,61 @@ public class SettingsScreen extends JPanel {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		tfName.setPreferredSize(new Dimension(30,20));
-//		tfName.setBounds(30, 30,10,10);
+		lblSettings.setFont(fontHead);
+		lblSettings.setForeground(Color.white);
+		lblSettings.setBounds(10, 10, 300, 50);
+
+		lblUserName.setFont(fontText);
+		lblUserName.setForeground(Color.WHITE);
+		lblUserName.setBounds(30, 85, 300, 20);
+
 		tfName.setBounds(300, 85, 100, 20);
-		frame.getContentPane().add(tfName);
+
+		lblControls.setFont(fontText);
+		lblControls.setForeground(Color.WHITE);
+		lblControls.setBounds(30, 120, 300, 20);
+
+		lblUp.setFont(fontTextSmall);
+		lblDown.setFont(fontTextSmall);
+		lblLeft.setFont(fontTextSmall);
+		lblRight.setFont(fontTextSmall);
+
+		lblUp.setForeground(Color.white);
+		lblLeft.setForeground(Color.white);
+		lblDown.setForeground(Color.white);
+		lblRight.setForeground(Color.white);
+
+		lblUp.setBounds(200, 150, 300, 20);
+		lblLeft.setBounds(200, 170, 300, 20);
+		lblDown.setBounds(200, 190, 300, 20);
+		lblRight.setBounds(200, 210, 300, 20);
+
+//		btnChange.setPreferredSize(new Dimension(50, 20));
+//		btnChange.setBounds(100, 150, 500, 200);
+		add(btnChange);
+
+		add(lblSettings);
+		add(lblUserName);
 		add(tfName);
+		add(lblControls);
+		add(lblUp);
+		add(lblLeft);
+		add(lblDown);
+		add(lblRight);
+
 		repaint();
-		
-		
-		
+
 	}
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
-		g.setFont(fontHead);
-		g.setColor(Color.white);
-		g.drawString("SETTINGS", 30, 50);
-		g.setFont(fontText);
-		g.drawString("Change Username" , 30, 100);
-		
+		// g.setFont(fontHead);
+		// g.setColor(Color.white);
+		// g.drawString("SETTINGS", 30, 50);
+		// g.setFont(fontText);
+		// g.drawString("Change Username" , 30, 100);
+
 	}
 
 	public static void main(String[] args) {
