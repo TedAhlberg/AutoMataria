9dbf38e623171e65b11995813302aad8aea3b5ba
lundow <eriklundow@gmail.com>
2018-04-10 11:30:54 +0200

    Merge

--- src/gameclient/TestStartScreen.java
+++ src/gameclient/TestStartScreen.java
@@ -17,8 +17,7 @@ import javax.swing.SwingConstants;
  * @author Henrik Olofsson
  */
 public class TestStartScreen extends JPanel {
-<<<<<<< HEAD
-    
+
     private BufferedImage bfImage;
     private String playFilenamePressed = "images/Play_Pressed.png";
     private String playFilenameUnpressed = "images/Play_Unpressed.png";
@@ -29,10 +28,15 @@ public class TestStartScreen extends JPanel {
     private String settingsFilenamePressed = "images/Settings_Pressed.png";
     private String settingsFilenameUnpressed = "images/Settings_Unpressed.png";
 
+    private String highScoresFilenamePressed = "images/Highscore_Pressed.png";
+    private String highScoresFilenameUnpressed = "images/Highscore_Unpressed.png";
     private Buttons btnPlay = new Buttons(playFilenamePressed, playFilenameUnpressed);
     private Buttons btnBrowse = new Buttons(browseFilenamePressed, browseFilenameUnpressed);
     private Buttons btnCreate = new Buttons(createFilenamePressed, createFilenameUnpressed);
     private Buttons btnSettings = new Buttons(settingsFilenamePressed, settingsFilenameUnpressed);
+    private Buttons btnHighScore = new Buttons(highScoresFilenamePressed, highScoresFilenameUnpressed);
+
+    private JPanel pnlGrid = new JPanel(new GridLayout(1, 5, 0, 50));
 
     public TestStartScreen() {
 
@@ -44,118 +48,49 @@ public class TestStartScreen extends JPanel {
 
         btnPlay.setMinimumSize(new Dimension(120, 120));
         btnPlay.setPreferredSize(new Dimension(120, 120));
-        add(btnPlay);
+        // add(btnPlay);
+        pnlGrid.add(btnPlay);
 
         btnBrowse.setMinimumSize(new Dimension(120, 120));
         btnBrowse.setPreferredSize(new Dimension(120, 120));
-        add(btnBrowse);
+        // add(btnBrowse);
+        pnlGrid.add(btnBrowse);
 
         btnCreate.setMinimumSize(new Dimension(120, 120));
         btnCreate.setPreferredSize(new Dimension(120, 120));
-        add(btnCreate);
+        // add(btnCreate);
+        pnlGrid.add(btnCreate);
 
         btnSettings.setMinimumSize(new Dimension(120, 120));
         btnSettings.setPreferredSize(new Dimension(120, 120));
-        add(btnSettings);
+        // add(btnSettings);
+        pnlGrid.add(btnSettings);
+
+        btnHighScore.setMinimumSize(new Dimension(120, 120));
+        btnHighScore.setPreferredSize(new Dimension(120, 120));
+        // add(btnHighScore);
+        pnlGrid.add(btnHighScore);
+        pnlGrid.setOpaque(false);
+
+        add(pnlGrid);
 
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        btnPlay.setLocation(40, getHeight() - btnPlay.getHeight() - 30);
         g.drawImage(bfImage, 0, 0, getWidth(), getHeight(), null);
+        pnlGrid.setLocation(getWidth() / 5, getHeight() - getHeight() / 4);
 
-        btnBrowse.setLocation(220, getHeight() - btnBrowse.getHeight() - 30);
-        btnCreate.setLocation(400, getHeight() - btnCreate.getHeight() - 30);
-        btnSettings.setLocation(580, getHeight() - btnCreate.getHeight() - 30);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setMinimumSize(new Dimension(400, 400));
-        frame.setPreferredSize(new Dimension(800, 800));
+        frame.setPreferredSize(new Dimension(930, 800));
         TestStartScreen sc = new TestStartScreen();
         frame.add(sc);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-=======
-	private BufferedImage bfImage;
-	private String playFilenamePressed = "images/Play_Pressed.png";
-	private String playFilenameUnpressed = "images/Play_Unpressed.png";
-	private String browseFilenamePressed = "images/Browse_Pressed.png";
-	private String browseFilenameUnpressed = "images/Browse_Unpressed.png";
-	private String createFilenamePressed = "images/Create_Pressed.png";
-	private String createFilenameUnpressed = "images/Create_Unpressed.png";
-	private String settingsFilenamePressed = "images/Settings_Pressed.png";
-	private String settingsFilenameUnpressed = "images/Settings_Unpressed.png";
-	private String highScoresFilenamePressed = "images/Highscore_Pressed.png";
-	private String highScoresFilenameUnpressed = "images/Highscore_Unpressed.png";
-	
-	private Buttons btnPlay = new Buttons(playFilenamePressed, playFilenameUnpressed);
-	private Buttons btnBrowse = new Buttons(browseFilenamePressed, browseFilenameUnpressed);
-	private Buttons btnCreate = new Buttons(createFilenamePressed, createFilenameUnpressed);
-	private Buttons btnSettings = new Buttons(settingsFilenamePressed, settingsFilenameUnpressed);
-	private Buttons btnHighScore = new Buttons(highScoresFilenamePressed, highScoresFilenameUnpressed);
-	
-	private JPanel pnlGrid = new JPanel(new GridLayout(1,5,0,50));
-	
-	public TestStartScreen() {
-		
-		try {
-			bfImage = ImageIO.read(new File("images/Auto-Mataria.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		btnPlay.setMinimumSize(new Dimension(120, 120));
-		btnPlay.setPreferredSize(new Dimension(120, 120));
-//		add(btnPlay);
-		pnlGrid.add(btnPlay);
-		
-		btnBrowse.setMinimumSize(new Dimension(120,120));
-		btnBrowse.setPreferredSize(new Dimension(120,120));
-//		add(btnBrowse);
-		pnlGrid.add(btnBrowse);
-		
-		btnCreate.setMinimumSize(new Dimension(120,120));
-		btnCreate.setPreferredSize(new Dimension(120,120));
-//		add(btnCreate);
-		pnlGrid.add(btnCreate);
-		
-		btnSettings.setMinimumSize(new Dimension(120,120));
-		btnSettings.setPreferredSize(new Dimension(120,120));
-//		add(btnSettings);
-		pnlGrid.add(btnSettings);
-		
-		btnHighScore.setMinimumSize(new Dimension(120,120));
-		btnHighScore.setPreferredSize(new Dimension(120,120));
-//		add(btnHighScore);
-		pnlGrid.add(btnHighScore);
-		pnlGrid.setOpaque(false);
-		
-		add(pnlGrid);
-		
-	}
-	
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(bfImage, 0, 0, getWidth(), getHeight(), null);
-		pnlGrid.setLocation(getWidth()/5, getHeight() - getHeight()/4);
-		
-	}
-
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setMinimumSize(new Dimension(400,400));
-		frame.setPreferredSize(new Dimension(930,800));
-		TestStartScreen sc = new TestStartScreen();
-		frame.add(sc);
-		frame.pack();
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
->>>>>>> 0f08a0fe54ba9e72828ead024c66c5e14e47ddd2
 }
