7ae0179ff6e67b7537b898438fe8a9e59e9c755e
tillbaks <johannes@bluml.se>
2018-04-22 17:40:03 +0200

    Fixed server script.

--- scripts/cloneCompileAndRun.sh
+++ scripts/cloneCompileAndRun.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Get sources
+# Get new source code
+rm -rf ./AutoMataria
 git clone https://github.com/TedAhlberg/AutoMataria.git
 
-# Go to cloned directory
-cd AutoMataria
+cd ./AutoMataria
+mkdir ./bin
 
 # Compile all sourcecode
-javac -encoding UTF-8 -d bin -sourcepath src -cp lib/mp3plugin.jar  -classpath src/ $(find . -name "*.java")
+javac -encoding UTF-8 -d ./bin -sourcepath ./src -cp ./lib/mp3plugin.jar -classpath ./src $(find ./ -name "*.java")
 
 # Start the server
-java -cp bin/ test.StartServer "Auto-Mataria Test Server 1" 32000 50 2 25 "Huge Map 1"
+java -cp ./bin/ test.StartServer "Auto-Mataria Test Server 1" 32000 50 2 25 "Small Map 1"
+
