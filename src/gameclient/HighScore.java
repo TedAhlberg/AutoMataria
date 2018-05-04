fffe5c0da9f4d242707fa70dc6b8e3616691c7b9
Henkomannen <henke94_olofsson@hotmail.com>
2018-05-04 14:15:29 +0200

    HighScore, HighScore.ser, HighScoreHandler
    HighScore, HighScore.ser, HighScoreHandler

---
+++ src/gameclient/HighScore.java
@@ -0,0 +1,30 @@
+package gameclient;
+
+import java.util.HashMap;
+
+public class HighScore {
+    private HashMap<String, Integer> highScores = new HashMap<>();
+    
+    public synchronized void put(String key, Integer value) {
+        highScores.put(key, value);
+    }
+    
+    public synchronized Integer get(String key) {
+        Integer value = highScores.get(key);
+        return value;
+    }
+    
+    public synchronized boolean containsKey(String key) {
+      return highScores.containsKey(key);
+    }
+    
+    public synchronized void replace(String key, String newUserName) {
+        int highScore = highScores.get(key);
+        highScores.remove(key);
+        highScores.put(newUserName, highScore);
+    }
+    
+    
+    
+
+}
---
+++ src/gameclient/HighScoreHandler.java
@@ -0,0 +1,116 @@
+package gameclient;
+
+import java.io.BufferedInputStream;
+
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 
+ * @author Henrik Olofsson
+ *  A class that will be used for writing and reading highscores.
+ *  And communicating with the main server.
+ *
+ */
+
+public class HighScoreHandler {
+    String fileName = "HighScores/HighScore.ser";
+    private Object object;
+    private HighScore highScores;
+    private boolean running = false;
+    private HighScoreServer thread = null;
+
+    public HighScoreHandler() {}
+
+    public synchronized void writeToFile(String userName, int highScore) {
+        highScores = readFromFile();
+
+        if(highScore > highScores.get(userName)) {
+            try (ObjectOutputStream writer = new ObjectOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(fileName)))){
+                highScores.put(userName, highScore);
+                writer.writeObject(highScores);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }        
+
+    }
+
+    public HighScore readFromFile() {
+
+        try (ObjectInputStream reader = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(fileName)))){
+            object = reader.readObject();
+            object = (HighScore) highScores; 
+        } catch (IOException e) {
+            e.getStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return highScores;
+    }
+
+    public void started() {
+        try {
+            thread = new HighScoreServer(10500);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        running = true;
+        thread.start();
+    }
+
+    public void stopped() {
+        running = false;
+        thread = null;
+    }
+
+    private class HighScoreServer extends Thread {
+        private ServerSocket serverSocket;
+        
+        public HighScoreServer(int port) throws IOException {
+            serverSocket = new ServerSocket(port);
+        }
+
+        public void run() {
+            while(running) {
+                System.out.println("Server running: " + running);
+                try (Socket socket = serverSocket.accept();
+                     DataInputStream fromMainServer =
+                             new DataInputStream(socket.getInputStream());
+                     DataOutputStream toMainServer =
+                             new DataOutputStream(socket.getOutputStream())){
+                    
+                    System.out.println("Server running on port: " + socket.getPort());
+                    
+                    String userName = fromMainServer.readUTF();
+                    int highScore = fromMainServer.readInt();
+                    
+                    writeToFile(userName,highScore);
+                         
+                     } catch(IOException e) {
+                         e.getStackTrace();
+                     }
+            }
+
+        }
+    }
+    
+    public static void main(String[] args) {
+        HighScoreHandler hsh = new HighScoreHandler();
+        hsh.started();
+    }
+}
