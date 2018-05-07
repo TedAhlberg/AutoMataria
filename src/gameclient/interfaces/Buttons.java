a672bdd6e1eaf1ffbc3a0fa491c07c751a782569
tillbaks <johannes@bluml.se>
2018-05-07 14:57:14 +0200

    ???

---
+++ src/gameclient/interfaces/Buttons.java
@@ -0,0 +1,65 @@
+package gameclient.interfaces;
+
+import gameclient.Resources;
+import gameclient.sound.SoundFx;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Henrik Olofsson & Erik Lundow
+ */
+public class Buttons extends JButton implements MouseListener {
+    private BufferedImage imagePressed;
+    private BufferedImage imageUnpressed;
+    private boolean isPressed = false;
+
+    public Buttons(String text) {
+        this("Blank_Pressed.png", "Blank_Unpressed.png");
+        setText(text);
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    }
+
+    public Buttons(String filenamePressed, String filenameUnpressed) {
+
+        imagePressed = Resources.getButtonImage(filenamePressed);
+        imageUnpressed = Resources.getButtonImage(filenameUnpressed);
+
+        setFocusPainted(false);
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorder(null);
+        setForeground(Color.WHITE);
+
+        addMouseListener(this);
+    }
+
+    public void mouseClicked(MouseEvent event) {}
+
+    public void mouseEntered(MouseEvent event) {}
+
+    public void mouseExited(MouseEvent event) {}
+
+    public void mousePressed(MouseEvent event) {
+        SoundFx.getInstance().menuSelect();
+        isPressed = true;
+        repaint();
+    }
+
+    public void mouseReleased(MouseEvent event) {
+        isPressed = false;
+        repaint();
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (isPressed) {
+            g.drawImage(imagePressed, 0, 0, getWidth(), getHeight(), null);
+        } else {
+            g.drawImage(imageUnpressed, 0, 0, getWidth(), getHeight(), null);
+        }
+        super.paintComponent(g);
+    }
+}
