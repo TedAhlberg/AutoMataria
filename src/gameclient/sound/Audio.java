83a2d79e29de2604b5e221d08bc86b640266410e
tillbaks <johannes@bluml.se>
2018-05-13 13:06:35 +0200

    Only load sound effects once at start of game.

--- src/gameclient/sound/Audio.java
+++ src/gameclient/sound/Audio.java
@@ -2,49 +2,41 @@ package gameclient.sound;
 
 import gameclient.Resources;
 
+import javax.sound.sampled.*;
+import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * A class for playing a sound clip.
- * 
+ *
  * @Erik Lundow
  */
 public class Audio implements Runnable {
-    
+    private static final Object lock = new Object();
+    private static boolean on = true;
+    private static boolean sfxOff = false;
+    private static boolean musicStopped;
+    private final int BUFFERSIZE = 4096;
     private SourceDataLine line;
     private AudioInputStream currentDecoded;
     private AudioInputStream encoded;
     private AudioFormat encodedFormat;
     private AudioFormat decodedFormat;
-    private static final Object lock = new Object();
     private boolean started = false;
     private boolean stopped = false;
     private boolean paused = false;
-    private static boolean on = true;
-    private static boolean sfxOff = false;
-    private static boolean musicStopped;
     private int times;
-    private final int BUFFERSIZE = 4096;
-    private URL url;
+    private File file;
+    private Thread soundThread;
 
     /**
-     * Constructs a Sound object specified by the url argument
-     * 
-     * @param url
-     *            giving the location of the sound file
+     * Constructs a Sound object specified by the file argument
+     *
+     * @param file giving the location of the sound file
      */
-    private Audio(URL url) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-        this.url = url;
-        encoded = AudioSystem.getAudioInputStream(url);
+    private Audio(File file) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        this.file = file;
+        encoded = AudioSystem.getAudioInputStream(file);
         encodedFormat = encoded.getFormat();
         decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, // Encoding to use
                 encodedFormat.getSampleRate(), // sample rate (same as base format)
@@ -62,25 +54,45 @@ public class Audio implements Runnable {
     }
 
     public static Audio getTrack(String filename) {
-        Audio sound = null;
         try {
-            URL url = Resources.musicPath.resolve(filename).toUri().toURL();
-            sound = new Audio(url);
+            return new Audio(Resources.musicPath.resolve(filename).toFile());
         } catch (Exception e) {
             System.out.println(e);
+            return null;
         }
-        return sound;
     }
 
     public static Audio getSFX(String filename) {
-        Audio sound = null;
         try {
-            URL url = Resources.sfxPath.resolve(filename).toUri().toURL();
-            sound = new Audio(url);
+            return new Audio(Resources.sfxPath.resolve(filename).toFile());
         } catch (Exception e) {
             System.out.println(e);
+            return null;
         }
-        return sound;
+    }
+
+    public static void setMusicStopped() {
+        musicStopped = true;
+        musicStopped = false;
+    }
+
+    public static void sfxOff() {
+        sfxOff = !sfxOff;
+    }
+
+    public static void on() {
+        setMusicStopped();
+        on = !on;
+        try {
+            lock.notifyAll();
+        } catch (Exception e) {}
+
+    }
+
+    public static void main(String[] args) {
+        Audio audio = Audio.getTrack("AM-GameTrack.mp3");
+        audio.play(1);
+//        audio.setVolume(1);
     }
 
     /**
@@ -88,29 +100,33 @@ public class Audio implements Runnable {
      * sound can only be started once.
      */
     public synchronized void play(int times) {
-       if(!on) {
-           try {
-            lock.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (!on) {
+            try {
+                lock.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-       }
         if (!started && on) {
             setLoop(times);
             new Thread(this).start();
             started = true;
         }
     }
-    
+
     /**
-     * Begins to play the sfx / resumes playback of a sound that is paused. A
-     * sound can only be started once.
+     * Begins to play the sfx / resumes playback of a sound that is paused.
      */
     public synchronized void playSfx() {
-        if(!started&&!sfxOff) {
+        if (!sfxOff) {
             setLoop(1);
-            new Thread(this).start();
-            started = true;
+            if (soundThread == null) {
+                started = true;
+                soundThread = new Thread(this);
+                soundThread.start();
+            } else {
+                resume();
+            }
         }
     }
 
@@ -121,11 +137,6 @@ public class Audio implements Runnable {
         setStopped(true);
         setPaused(false);
         notify();
-    }
-    
-    public static void setMusicStopped() {
-        musicStopped=true;
-        musicStopped=false;
     }
 
     /**
@@ -192,20 +203,22 @@ public class Audio implements Runnable {
             try {
                 i = currentDecoded.read(b, 0, b.length);
                 if (i == -1) {
-                    if (times > 1) {
+                    if (times >= 1) {
                         times--;
                         try {
-                            encoded = AudioSystem.getAudioInputStream(url);
+                            encoded = AudioSystem.getAudioInputStream(file);
                             currentDecoded = AudioSystem.getAudioInputStream(decodedFormat, encoded);
                         } catch (UnsupportedAudioFileException e) {
                             setStopped(true);
                             e.printStackTrace();
                         }
-                    } else {
-                        setStopped(true);
                     }
-                } else
+                } else {
                     line.write(b, 0, i);
+                }
+                if (times == 0) {
+                    setPaused(true);
+                }
             } catch (IOException e) {
                 System.err.println(e);
             }
@@ -219,28 +232,5 @@ public class Audio implements Runnable {
         } catch (IOException e) {
             System.err.println(e);
         }
-
     }
-
-    public static void sfxOff() {
-        sfxOff = !sfxOff;
-        
-    }
-    
-    public static void on() {
-        setMusicStopped();
-        on=!on;
-        try {
-        lock.notifyAll();
-        }catch (Exception e){}
-        
-    }
-
-    public static void main(String[] args) {
-        Audio audio = Audio.getTrack("AM-GameTrack.mp3");
-        audio.play(1);
-//        audio.setVolume(1);
-
-    }
-
 }
