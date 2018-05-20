227c806381c44f1d7c116cedc54eae1616b35107
tillbaks <johannes@bluml.se>
2018-05-20 15:28:05 +0200

    Fix server script

--- scripts/cloneCompileAndRun.sh
+++ scripts/cloneCompileAndRun.sh
@@ -11,5 +11,5 @@ mkdir ./bin
 javac -encoding UTF-8 -d ./bin -sourcepath ./src -cp ./lib/mp3plugin.jar -classpath ./src $(find ./ -name "*.java")
 
 # Start the server
-java -cp ./bin/ test.StartServer "Auto-Mataria Test Server 1" 32000 50 2 50
+java -cp ./bin/ GameServer.StartServer "Auto-Mataria Test Server 1" 32000 50 2 50
 
