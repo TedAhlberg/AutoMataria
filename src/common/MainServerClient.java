357158b6c3ce4e0e3a6395a2eb3455e998ed0dba
tillbaks <johannes@bluml.se>
2018-05-22 12:39:42 +0200

    Fix problem with mainserverclient. inputstream is required to be connected even if it is not used.

--- src/common/MainServerClient.java
+++ src/common/MainServerClient.java
@@ -8,19 +8,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainServerClient {
-	
-	private MainServerClient() {
-		
-	}
+
+    private MainServerClient() {
+
+    }
 
     public static void sendServerInformation(ServerInformation serverInformation) {
         try (Socket socket = new Socket()) {
             socket.connect(Game.MAIN_SERVER, 500);
-            try(ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
                 outputStream.writeObject("SET_GAMESERVER");
                 outputStream.writeObject(serverInformation);
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.getStackTrace();
         }
     }
@@ -28,20 +29,21 @@ public class MainServerClient {
     public static void sendGameScore(HashMap<String, Integer> scores) {
         try (Socket socket = new Socket()) {
             socket.connect(Game.MAIN_SERVER, 500);
-            try(ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
                 outputStream.writeObject("SET_HIGHSCORES");
                 outputStream.writeObject(scores);
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.getStackTrace();
         }
     }
-    
+
     public static ArrayList<HighScore2> getHighscores() {
         try (Socket socket = new Socket()) {
             socket.connect(Game.MAIN_SERVER, 500);
-            try(ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-                    ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
                 outputStream.writeObject("GET_HIGHSCORES");
                 return (ArrayList<HighScore2>) inputStream.readObject();
             }
@@ -50,11 +52,11 @@ public class MainServerClient {
             return null;
         }
     }
-    
+
     public static ArrayList<ServerInformation> getServers() {
         try (Socket socket = new Socket();
-                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
+             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
             socket.connect(Game.MAIN_SERVER, 500);
             outputStream.writeObject("GET_SERVERS");
             return (ArrayList<ServerInformation>) inputStream.readObject();
