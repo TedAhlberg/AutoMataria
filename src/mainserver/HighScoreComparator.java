7e5acffd0650e5701dcfcb67dd781d9c42a76e27
Henkomannen <henke94_olofsson@hotmail.com>
2018-05-24 09:41:44 +0200

    highscorecomparator

--- src/mainserver/HighScoreComparator.java
+++ src/mainserver/HighScoreComparator.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class HighscoreComparator implements Comparator<Highscore> {
 
     public int compare(Highscore highscore, Highscore highscore2) {
-        return highscore.getHighscore() - highscore2.getHighscore();
+        return highscore2.getHighscore() - highscore.getHighscore();
     }
 }
